3e1a2c24871b2ee30d761c35f6405f3c2345a4e4
Roman52 <ser-jar1990@yandex.ru>
2020-04-04 10:26:40 +0300

    lesson 56

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
-const finalGrade = (exam, projects) => {
-    let res = 0;
 
-    if (exam > 90 || projects > 10) {
-        res = 100;
-    } else if (exam > 75 && projects >= 5) {
-        res = 90;
-    } else if (exam > 50 && projects >= 2) {
-        res = 75;
-    }
+// lesson 56
+/*
+Реализуйте функцию, которая принимает на вход номер кредитки (состоящей из 16 цифр) и возвращает его скрытую версию, которая может использоваться на сайте для отображения. Если исходная карта имела номер 2034399002125581, то скрытая версия выглядит так ****5581. Другими словами, функция заменяет первые 12 символов, на четыре звездочки. Количество звездочек регулируется вторым необязательным параметром. Значение по умолчанию - 4.
+ */
 
-    return res;
+/*
+const getHiddenCard = (cardNumber, numsCount = 4) => {
+    const hiddenCardNumber = cardNumber.toString().slice(12);
+    return hiddenCardNumber.padStart(hiddenCardNumber.length + numsCount, '*');
 }
+
+var cardNumber = getHiddenCard(1234567812345678, 2);
+console.log(cardNumber);
+
+ */
+
+
+
 
 
 
